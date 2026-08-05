81c925be5fc75103868aeda446ab9829a05c18fc
Hala M. Dalloul <haladalloul42@gmail.com>
2026-08-05 20:27:22 +0300

    Update controller attribute

--- lib/SignIn&Up/CustomTextField.dart
+++ lib/SignIn&Up/CustomTextField.dart
@@ -3,22 +3,29 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final String hintText;
   final Widget? suffixIcon;
+  final Function(String)? onValueChanged;
+  final TextEditingController? controller;
 
   const CustomTextField({
     super.key,
     required this.hintText,
     this.suffixIcon,
+    this.onValueChanged,
+    this.controller
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
+      onChanged: onValueChanged,
       decoration: InputDecoration(
         hintText: hintText,
         hintStyle: const TextStyle(color: Colors.grey, fontSize: 14),
         contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
         suffixIcon: suffixIcon,
         suffixIconColor: Colors.grey,
+
 
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(25),
