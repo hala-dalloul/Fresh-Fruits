84b0895db4b67e528d53243458012c6988833833
Hala M. Dalloul <haladalloul42@gmail.com>
2026-08-05 13:42:26 +0300

    Revert "Splash&onboard"

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,12 +9,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:fresh_fruits_final/main.dart';
-import 'package:fresh_fruits_final/splash&onboard/SplashScreen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const SplashScreen());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
